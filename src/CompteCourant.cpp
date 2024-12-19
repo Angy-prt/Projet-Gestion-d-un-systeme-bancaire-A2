d7e876078e5ad2b0a54d34ed460146411ef5f36d
Angy-prt <angelique.porte64@gmail.com>
2024-12-19 19:41:24 +0100

    Create CompteCourant.cpp

---
+++ src/CompteCourant.cpp
@@ -0,0 +1,32 @@
+#include "CompteCourant.h"
+#include <iostream>
+
+CompteCourant::CompteCourant() : CompteBancaire(), decouvertAutorise(0.0) {}
+
+CompteCourant::CompteCourant(const std::string& titulaire, double solde, int numeroCompte, double decouvertAutorise)
+    : CompteBancaire(titulaire, solde, numeroCompte), decouvertAutorise(decouvertAutorise) {}
+
+CompteCourant::CompteCourant(const CompteCourant& autre)
+    : CompteBancaire(autre), decouvertAutorise(autre.decouvertAutorise) {}
+
+CompteCourant::~CompteCourant() {}
+
+double CompteCourant::getDecouvertAutorise() const { return decouvertAutorise; }
+
+void CompteCourant::setDecouvertAutorise(double montant) { decouvertAutorise = montant; }
+
+bool CompteCourant::retirer(double montant) {
+    if (solde + decouvertAutorise >= montant) {
+        solde -= montant;
+        return true;
+    }
+    std::cout << "Fonds insuffisants (découvert inclus)." << std::endl;
+    return false;
+}
+
+
+
+void CompteCourant::afficherInfo() const {
+    CompteBancaire::afficherInfo();
+    std::cout << "Decouvert Autorise: " << decouvertAutorise << " EUR" << std::endl;
+}
