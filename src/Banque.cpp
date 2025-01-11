2a22f521a19647fc1d1a829b9ae84676b637a09a
Angy-prt <angelique.porte64@gmail.com>
2025-01-11 13:03:00 +0100

    Update Banque.cpp

--- src/Banque.cpp
+++ src/Banque.cpp
@@ -1,6 +1,7 @@
 #include "Banque.h"
 #include "CompteCourant.h"
 #include "CompteEpargne.h"
+#include <iostream>
 
 Banque::Banque() {
     initialiserComptes();
@@ -16,7 +17,7 @@ void Banque::ajouterCompte(CompteBancaire* compte) {
     comptes.push_back(compte);
 }
 
-CompteBancaire* Banque::rechercherCompteParNumero(int numero) {
+CompteBancaire* Banque::rechercherCompteParNumero(int numero) const {
     for (auto compte : comptes) {
         if (compte->getNumeroCompte() == numero) {
             return compte;
@@ -26,6 +27,11 @@ CompteBancaire* Banque::rechercherCompteParNumero(int numero) {
 }
 
 void Banque::afficherComptes() const {
+    if (comptes.empty()) {
+        std::cout << "Aucun compte dans la banque.\n";
+        return;
+    }
+
     for (auto compte : comptes) {
         compte->afficherInfo();
     }
@@ -35,5 +41,5 @@ void Banque::initialiserComptes() {
     comptes.push_back(new CompteCourant("Alice Dupont", 1500.0, 10000001, 500.0));
     comptes.push_back(new CompteEpargne("Alice Dupont", 5000.0, 10000002, 3.0));
     comptes.push_back(new CompteCourant("Bob Martin", 1200.0, 10000003, 300.0));
-    comptes.push_back(new CompteEpargne("Bob Martin", 3000.0, 10000004, 2.5));  
+    comptes.push_back(new CompteEpargne("Bob Martin", 3000.0, 10000004, 2.5));
 }
